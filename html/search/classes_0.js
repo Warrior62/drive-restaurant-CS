cc37ec578f92710e27e218682787693b897bede2
Warrior62 <50228030+Warrior62@users.noreply.github.com>
2021-02-17 22:29:54 +0100

    Delete html directory

--- html/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['param_5fthread_5ft',['param_thread_t',['../structparam__thread__t.html',1,'']]],
-  ['produit_5ft',['produit_t',['../structproduit__t.html',1,'']]]
-];
--- html/search/classes_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['reponse_5ft',['reponse_t',['../structreponse__t.html',1,'']]],
-  ['requete_5ft',['requete_t',['../structrequete__t.html',1,'']]]
-];
